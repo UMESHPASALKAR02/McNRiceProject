9bee7c8aa2436eaa78273618c154ab03c5291726
UMESH <umeshpasalkar@gmail.com>
2020-01-02 18:15:51 +0530

    Initial commit

---
+++ McNRiceProject/McNRiceProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  McNRiceProject
+//
+//  Created by MAYURI on 1/2/20.
+//  Copyright © 2020 umesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var idTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+        
+    }
+
+
+}
+
